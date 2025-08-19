f2b5498e71b1cf901665d680b86be47fb7fb44e4
David Grandas <ricardos.gc123@gmail.com>
2025-08-19 17:15:07 -0500

    feat: Cambiar imagen de contacto por imagen5 de impermeabilización
     CAMBIO REALIZADO:
    
     IMAGEN DE CONTACTO ACTUALIZADA:
    - Cambiar imagen3.jpeg  imagen5.jpeg
    - Sección: Contáctanos
    - Categoría: Impermeabilización
    - Mejor representación visual del servicio
    
     RESULTADO:
    - Imagen más representativa en contacto
    - Mejor impacto visual
    - Consistencia con el portafolio de impermeabilización

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,9 +9,5 @@ import { RouterLink } from '@angular/router';
   styleUrl: './hero.scss'
 })
 export class HeroComponent {
-  @Output() openAppointmentPopup = new EventEmitter<void>();
 
-  openAppointment() {
-    this.openAppointmentPopup.emit();
-  }
 }
